06509379fa6ec23f3e0cf96c063095933f8f7c59
samidegraeve <91418159+samidegraeve@users.noreply.github.com>
2024-09-10 22:05:39 +0200

    aa

--- src/expender/expender.c
+++ src/expender/expender.c
@@ -6,13 +6,16 @@
 /*   By: sadegrae <sadegrae@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/14 18:30:18 by sadegrae          #+#    #+#             */
-/*   Updated: 2024/09/10 20:11:58 by sadegrae         ###   ########.fr       */
+/*   Updated: 2024/09/10 22:02:51 by sadegrae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
 
-
+int check_quote_not_open(char *str)
+{
+	echo 
+}
 
 void	ft_expend(t_stack *stack, t_env *env)
 {
@@ -25,7 +28,7 @@ void	ft_expend(t_stack *stack, t_env *env)
 	while (str)
 	{
 		printf("str = %s\n", str->content);
-
+		
 		if (check_if_dollard(str->content) == 1)
 			if_or_not_convert(str, env);
 		ft_split_cot(str);
